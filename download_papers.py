4d62486618f4c44543eac658be21c0ff806dbdd0
tanmaysachan <tnmysachan@gmail.com>
2019-09-29 00:55:02 +0530

    chhajer chutiya

--- download_papers.py
+++ download_papers.py
@@ -1,14 +1,16 @@
 import arxiv
 import os
 
-query = "chemistry"
+query = "iot"
 max_results = 500
 
 os.mkdir("test_papers")
 os.chdir("test_papers")
 
-papers = arvix.query(query = query, max_results = max_results)
+papers = arxiv.query(query = query, max_results = max_results)
 
+cnt = 0;
 for paper in papers:
+    print("downloading ", cnt);
+    cnt += 1;
     arxiv.download(paper)
-
